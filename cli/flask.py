f74aac24c99383bfd770c00c65254b0894377d51
Prakasa TKPD <nedya.prakasa@tokopedia.com>
2019-07-18 01:01:16 +0700

    fixing init db on bootstrap

--- cli/flask.py
+++ cli/flask.py
@@ -20,14 +20,21 @@ def initdb():
 
     db.create_all()
 
-    # Register initial app to account
-    client_id = AccountClient(**init_account).save()
-    # Register initial member
-    member = AccountMember(**member_data_input_serializer(init_member)).save()
+    client = AccountClient.query.filter_by(
+        client_code=init_account["client_code"]).first()
 
-    # Save Member with client
-    AccountMembersClient(mc_client_id=client_id,
-                         mc_member_id=member, mc_is_approved=3).save()
+    # Check client exist or not
+    if client is None:
+        # Register initial app to account
+        client_id = AccountClient(**init_account).save()
+
+        # Register initial member
+        member = AccountMember(
+            **member_data_input_serializer(init_member)).save()
+
+        # Save Member with client
+        AccountMembersClient(mc_client_id=client_id,
+                             mc_member_id=member, mc_is_approved=3).save()
 
 
 @app.cli.command()
